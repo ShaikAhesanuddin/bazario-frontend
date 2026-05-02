6ed6367cdc82cc3c5893b9f6a4bcff5515f5a4a0
Shaik Ahesanuddin <ahesan3954@gmail.com>
2026-05-02 21:45:19 +0530

    Update API base URL to v1 for consistent routing in Netlify redirects and axios configuration

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import { toast } from "react-toastify";
 
 const api = axios.create({
-    baseURL: "/api"
+    baseURL: "/api/v1"
 });
 
 api.interceptors.request.use((config) => {
